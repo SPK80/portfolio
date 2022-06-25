a3c7d8ed887691b991eb6a90a575af7688a58eeb
SPK80 <psukonkin@gmail.com>
2022-06-25 23:44:18 +0300

    feat: add Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./header/Header";
+import {Main} from "./main/Main";
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
                 <Header/>
-            
+                <Main/>
             </div>
         );
     }
---
+++ src/main/Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './main.module.css'
+
+export const Main = () =>
+    <div className={s.main}>
+        <div className={s.container}>
+            <div className={s.text}>
+                <span>Hi!</span>
+                <h2>I am Pavel Sukonkin</h2>
+                <span>A front-end developer</span>
+            </div>
+            <div className={s.photo}>
+            
+            </div>
+        </div>
+    </div>
