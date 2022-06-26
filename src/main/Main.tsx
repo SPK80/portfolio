5b85baef7c83f3fb630fff3b68cba71f0d601d10
SPK80 <psukonkin@gmail.com>
2022-06-26 07:06:24 +0300

    refactor: move container styles to common

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import s from './main.module.css'
+import sContainer from '../common/styles/container.module.css'
 
 export const Main = () =>
     <div className={s.main}>
-        <div className={s.container}>
+        <div className={sContainer.container}>
             <div className={s.text}>
                 <span>Hi!</span>
                 <h2>I am Pavel Sukonkin</h2>
