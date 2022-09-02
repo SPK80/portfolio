24e8240f8bb7d3fa2a8b491c9148c00f9d489808
SPK80 <psukonkin@gmail.com>
2022-09-02 06:24:55 +0300

    feat: change main styles

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from './main.module.css'
 import sContainer from '../common/styles/container.module.css'
+import photo from '../assets/photo.jpg'
 
 export const Main = () =>
     <div id="main" className={s.main}>
         <div className={sContainer.container}>
             <div className={s.text}>
-                <span>Hi!</span>
-                <h2>I am Pavel Sukonkin</h2>
-                <span>A front-end developer</span>
+                <h3 className={s.hello}>Hi!</h3>
+                <h3 className={s.name}>I am Pavel Sukonkin</h3>
+                <h3 className={s.job}>A front-end developer</h3>
             </div>
-            <img className={s.photo} src={'https://avatars.githubusercontent.com/u/36849366?v=4'} alt={'photo'}/>
+            <img className={s.photo} src={photo} alt={'photo'}/>
         </div>
     </div>
