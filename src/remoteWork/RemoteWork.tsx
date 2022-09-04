5997013e73871e4d65185920fe571835cf8838d6
SPK80 <psukonkin@gmail.com>
2022-09-04 13:00:23 +0300

    refactor: RemoteWork

--- src/remoteWork/RemoteWork.tsx
+++ src/remoteWork/RemoteWork.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import s from './remoteWork.module.css'
-import sContainer from '../common/styles/container.module.css'
+import s from './remoteWork.module.scss'
 
 export const RemoteWork = () => (
-    <div id="remoteWork" className={s.remoteWorkBlock}>
-        <div className={`${sContainer.container} ${s.remoteWorkContainer}`}>
-            <h2>Looking into remote work options</h2>
-            <a href="#">Hire me</a>
-        </div>
+    <div className={s.remoteWorkContainer}>
+        <h2>Looking into remote work options</h2>
+        <a href="#">Hire me</a>
     </div>
 )
