658465cff9334d152ca5f2436f7f291e31a2fa50
SPK80 <psukonkin@gmail.com>
2022-09-04 20:17:19 +0300

    refactor: add projects images, create projectsData refactor Projects and Project

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -2,10 +2,42 @@ import React from "react";
 import {Project} from "./project/Project";
 import {ContainerWithTitle} from "../common/components/ContainerWithTitle/ContainerWithTitle";
 
-export const Projects = () => (
-    <ContainerWithTitle title="Projects" id="projects">
-        <Project title={"Samurai-way"} description={"Social network"}/>
-        <Project title={"Todolist"} description={"Todo list"}/>
-        <Project title={"Counter"} description={"Counter"}/>
-    </ContainerWithTitle>
-)
+type  ProjectDataType = {
+    title: string
+    description: string
+    image: string
+}
+
+export const Projects = () => {
+    const projectsData: ProjectDataType[] = [
+        {
+            title: "Samurai-way",
+            description: "Social network",
+            image: "social-network.jpg",
+        },
+        {
+            title: "Todolist",
+            description: "Todo list",
+            image: "todolist.jpg",
+        },
+        {
+            title: "Counter",
+            description: "Counter",
+            image: "counter.jpg",
+        },
+    ]
+    
+    return (
+        <ContainerWithTitle title="Projects" id="projects">
+            {
+                projectsData.map(pd =>
+                    <Project
+                        style={{backgroundImage: `url(${require('../assets/' + pd.image)})`}}
+                        title={pd.title}
+                        description={pd.description}
+                    />
+                )
+            }
+        </ContainerWithTitle>
+    )
+}
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import s from './project.module.scss'
 
 type SkillPropsType = {
     title: string
     description: string
+    style?: CSSProperties | undefined;
 }
 
-export const Project: React.FC<SkillPropsType> = ({title, description}) =>
+export const Project: React.FC<SkillPropsType> = ({title, description, style}) =>
     <div className={s.project}>
-        <div>
+        <div style={style}>
             <a href="#">Show</a>
         </div>
         <h3>{title}</h3>
