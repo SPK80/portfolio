d8ae647e0eeca1d14c9cb601d56e983c6561acdd
SPK80 <psukonkin@gmail.com>
2022-09-04 20:18:44 +0300

    feat: update ContactsInfo

--- src/contacts/ContactsInfo/ContactsInfo.tsx
+++ src/contacts/ContactsInfo/ContactsInfo.tsx
@@ -5,10 +5,13 @@ import {ContactItem} from "../ContactItem/ContactItem";
 export const ContactsInfo: React.FC = () =>
     <div className={s.contactsInfo}>
         <ContactItem title="Email">
-            psukonkin@gmail.com
+            <a href="mailto:psukonkin@gmail.com">psukonkin@gmail.com</a>
         </ContactItem>
         <ContactItem title="Phone">
-            +123 4567890
+            <a href="tel:+1234567890">+123 4567890</a>
+        </ContactItem>
+        <ContactItem title="Linkedin">
+            <a href="https://www.linkedin.com/in/sukonkin/">sukonkin</a>
         </ContactItem>
         <ContactItem title="Location">
             Earth
