2d0b6f91013367f2bfc1f8db178f96995b07914d
SPK80 <psukonkin@gmail.com>
2022-09-06 20:50:56 +0300

    refactor: Navbar

--- src/common/components/navbar/Navbar.tsx
+++ src/common/components/navbar/Navbar.tsx
@@ -1,23 +1,31 @@
 import React, {useState} from "react";
 import s from './navbar.module.scss'
-import {FiMenu} from "react-icons/fi";
+import {GrClose, GrMenu} from "react-icons/gr";
 
-export const LaptopNavbar = () => {
+export const Navbar = () =>
+    <div className={s.navbar}>
+        <div className={s.laptopNavbar}><LaptopNavbar/></div>
+        <div className={s.desktopNavbar}><DesktopNavbar/></div>
+    </div>
+
+
+const DesktopNavbar: React.FC = () => <NavMenu/>
+
+const LaptopNavbar: React.FC = () => {
     const [isMenuHidden, setIsMenuHidden] = useState(true)
-
+    
     const onClickHandler = () => {
-        setIsMenuHidden(false)
+        setIsMenuHidden(value => !value)
     }
-
-    return isMenuHidden
-        ? <button onClick={onClickHandler}><FiMenu/></button>
-        : <NavMenu/>
+    
+    return <>
+        {<button onClick={onClickHandler}>{isMenuHidden ? <GrMenu/> : <GrClose/>}</button>}
+        {!isMenuHidden && <NavMenu onClick={onClickHandler}/>}
+    </>
 }
 
-export const DesktopNavbar = () => <NavMenu/>
-
-const NavMenu: React.FC = () =>
-    <nav className={s.navMenu}>
+const NavMenu: React.FC<{ onClick?: () => void }> = ({onClick}) =>
+    <nav onClick={onClick}>
         <a href={"#main"}>Main</a>
         <a href={"#skills"}>Skills</a>
         <a href={"#projects"}>Projects</a>
