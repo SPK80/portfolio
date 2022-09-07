d9be259d92c667ad941049854d67a16006d2efd2
SPK80 <psukonkin@gmail.com>
2022-09-07 16:17:58 +0300

    refactor: add media queries in contacts style, tune contacts styles

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -5,8 +5,8 @@ import {ContactsInfo} from "./ContactsInfo/ContactsInfo";
 
 export const Contacts = () =>
     <div id="contacts" className={s.contactsContainer}>
-        <div className={s.encouragement}>
-            <div>Let's contact</div>
+        <div className={s.title}>
+            <div>Let's connect</div>
             <div>Drop Me a Line</div>
         </div>
         <div className={s.formAndInfoContainer}>
