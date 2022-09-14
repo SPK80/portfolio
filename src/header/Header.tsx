5992c67ab76e5548ec623cb60c0acec291bfe50d
SPK80 <psukonkin@gmail.com>
2022-09-14 13:33:19 +0300

    feat: update header and navbar, create logo

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import s from './header.module.scss'
 import {Navbar} from "../common/components/navbar/Navbar";
+import logo from "../assets/logo.jpg";
 
 export const Header = () =>
     <header className={s.header}>
+        <div className={s.logo}><img src={logo} alt=""/></div>
         <Navbar/>
     </header>
