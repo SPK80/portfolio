3678929f3e6487d4dbed6710cb4c594bacc38e5e
SPK80 <psukonkin@gmail.com>
2022-09-14 16:53:04 +0300

    refactor: wrap Skills layout

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import {Skill} from "./skill/Skill";
 import {ContainerWithTitle} from "../common/components/ContainerWithTitle/ContainerWithTitle";
 import {skillsData} from "../bll/skillsData";
+import s from "./skills.module.scss"
 
 export const Skills = () =>
     <ContainerWithTitle title="Skills" id="skills">
-        {
-            skillsData.map((sd, index) =>
-                <Skill
-                    key={index}
-                    skillData={sd}
-                />
-            )
-        }
+        <div className={s.skills}>
+            {
+                skillsData.map((sd, index) =>
+                    <Skill
+                        key={index}
+                        skillData={sd}
+                    />
+                )
+            }
+        </div>
     </ContainerWithTitle>
