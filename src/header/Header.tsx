c1e6ae2e22b1dc0bf710df9754fbd52b698ff248
SPK80 <psukonkin@gmail.com>
2022-09-16 06:43:58 +0300

    refactor: create NavMenu, add media query to header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from './header.module.scss'
-import {Navbar} from "../common/components/navbar/Navbar";
 import logo from "../assets/logo.jpg";
+import {NavMenu} from "./navMenu/NavMenu";
 
 export const Header = () =>
     <header className={s.header}>
         <div className={s.logo}><img src={logo} alt=""/></div>
-        <Navbar/>
+        {/*<Navbar/>*/}
+        <NavMenu/>
     </header>
